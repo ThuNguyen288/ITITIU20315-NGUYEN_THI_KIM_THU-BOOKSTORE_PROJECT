b785bdfe859298600746786fbd94a3ece0607740
Nguyễn Thu <ititiu20315@student.hcmiu.edu.vn>
2024-11-28 10:46:04 +0700

    Update AdminSidebar.js

--- src/app/components/AdminSidebar.js
+++ src/app/components/AdminSidebar.js
@@ -1,17 +1,19 @@
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { HomeIcon, BookOpenIcon, ShoppingBagIcon, UserIcon } from '@heroicons/react/24/outline';
+import { HomeIcon, BookOpenIcon, ShoppingBagIcon, UserIcon, BackwardIcon } from '@heroicons/react/24/outline';
 
 export default function Sidebar() {
   const pathname = usePathname();
   const [active, setActive] = useState('');
 
   const menuItems = [
-    { name: 'Dashboard', href: '/pages/main/admin/dashboard', icon: HomeIcon },
-    { name: 'Products', href: '/pages/main/admin/product', icon: BookOpenIcon },
+    { name: 'Dashboard', href: './dashboard', icon: HomeIcon },
+    { name: 'Products', href: './product', icon: BookOpenIcon },
     { name: 'Orders', href: '#', icon: ShoppingBagIcon },
     { name: 'Account', href: '#', icon: UserIcon },
+    { name: '', href: '../customer/dashboard', icon: BackwardIcon },
+
   ];
 
   useEffect(() => {
@@ -19,15 +21,15 @@ export default function Sidebar() {
   }, [pathname]);
 
   return (
-    <div className="h-full bg-gray-800 text-white">
+    <div className=" h-screen bg-gray-800 text-white">
       <div className="flex flex-col items-start p-4">
         {menuItems.map((item) => (
           <Link
             key={item.name}
             href={item.href}
             className={`flex items-center p-2 rounded-lg w-full mb-2 ${
-              active.startsWith(item.href)
-                ? 'bg-indigo-600 text-white'
+              pathname.startsWith(item.href)
+                ? 'bg-indigo-600 text-white'  // Active state when the route matches
                 : 'text-gray-300 hover:bg-gray-700 hover:text-white'
             }`}
           >
